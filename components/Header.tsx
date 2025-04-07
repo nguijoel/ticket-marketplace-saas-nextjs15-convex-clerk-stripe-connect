5dd757a843e095a162ddee69048ee36dca6954da
Gerald <nguijoel@gmail.com>
2025-04-07 01:45:57 +0100

    update logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,20 +10,20 @@ function Header() {
       <div className="flex flex-col lg:flex-row items-center gap-4 p-4">
         <div className="flex items-center justify-between w-full lg:w-auto">
           <Link href="/" className="font-bold shrink-0">
-            {/* <Image
+            <Image
               src={logo}
               alt="logo"
               width={100}
               height={100}
               className="w-24 lg:w-28"
-            />  */}
-            <Image
+            /> 
+            {/* <Image
               src="https://cms.ontoo.cloud/media/ue5edz4y/logo-3.png"
               alt="logo"
               width={100}
               height={100}
               className="w-24 lg:w-28"
-            />
+            /> */}
           </Link>
 
           <div className="lg:hidden">
